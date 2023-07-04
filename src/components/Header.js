1555fd217244b0c2bb5f534a98db107356955c6e
Luiz Bezerra <lhbezerra@hotmail.com>
2023-07-04 07:06:13 -0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import logo from '../images/mlabs-color-2 1.png';
+import avatar from '../images/Ellipse 1.png'
+
+
+function Header() {
+    return (
+        <>
+            <div className="grid-container">
+                <div className="item">   
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
+                <div className="item">
+                    <div className="user_name">
+                        <p>Anselmo Carlos</p>
+                    </div>
+                    <img src={avatar} className="avatar" alt="logo" />
+                </div>
+            </div>
+        </>
+       
+
+    );
+  }
+
+
+export default Header;
